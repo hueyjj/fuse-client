e3ae0c5e070907282bc56a0101def80486a7de23
hueyjj <jasperjeng@gmail.com>
2018-07-12 01:38:32 -0700

    Added method to fetch from localhost

--- chrome-ext/youtube-dl-ext/content-script.js
+++ chrome-ext/youtube-dl-ext/content-script.js
@@ -6,10 +6,31 @@ let injectMisterGrumpy = () => {
   var grumpyBtn = document.createElement("button");
   grumpyBtn.id = "mister-grumpy-button"
   grumpyBtn.textContent = "Mister Grumpy's button"
+  grumpyBtn.onclick = fetchMusicVideoStatus;
 
   let infoTextNode = document.querySelector("#info-text");
   infoTextNode.parentNode.insertBefore(grumpyBtn, infoTextNode.nextSibling);
+};
 
+let fetchMusicVideoStatus = () => {
+  fetch("http://localhost:8080/youtube/music/find", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({ "url": "foo" })
+  })
+    .then(resp => {
+      return resp.json();
+    })
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log("error");
+      console.log(err);
+    })
 };
 
 let id = setInterval(() => {

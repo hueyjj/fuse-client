aede0680ff3b41cf0656b7f807bedc1e0082ffed
hueyjj <jasperjeng@gmail.com>
2018-07-13 02:15:01 -0700

    Main functionalities working

--- chrome-ext/youtube-dl-ext/content-script.js
+++ chrome-ext/youtube-dl-ext/content-script.js
@@ -1,41 +1,110 @@
 // This script immediately runs when visiting a youtube url
 
-console.log("Inserting Mister Grumpy's needle...");
+const SEARCH_ENDPOINT = "http://localhost:8080/youtube/music/search";
+const DOWNLOAD_ENDPOINT = "http://localhost:8080/youtube/music/download";
+const MUSIC_NOT_EXIST = "MUSIC_NOT_EXIST";
+const MUSIC_EXIST = "MUSIC_EXIST";
+const DOWNLOAD_FAIL = "DOWNLOAD_FAIL";
+const DOWNLOAD_SUCCESS = "DOWNLOAD_SUCCESS";
+const URL = location.href;
 
-let injectMisterGrumpy = () => {
-  var grumpyBtn = document.createElement("button");
-  grumpyBtn.id = "mister-grumpy-button"
-  grumpyBtn.textContent = "Mister Grumpy's button"
-  grumpyBtn.onclick = fetchMusicVideoStatus;
-
-  let infoTextNode = document.querySelector("#info-text");
-  infoTextNode.parentNode.insertBefore(grumpyBtn, infoTextNode.nextSibling);
+var grumpyBtn = null;
+var searchStatus = null; 
+let clearBtnStatus = () => {
+  if (grumpyBtn) {
+    grumpyBtn.classList.remove("okay-status", "not-okay-status", "unknown-status");
+  }
+};
+let updateBtnStatus = () => {
+  clearBtnStatus();
+  if (grumpyBtn) {
+    if (searchStatus) {
+      if (searchStatus.status == MUSIC_NOT_EXIST) {
+        grumpyBtn.textContent = "Mister Grumpy Download";
+        grumpyBtn.classList.add("not-okay-status");
+      }
+      else if (searchStatus.status == MUSIC_EXIST) {
+        grumpyBtn.textContent = "Mister Grumpy";
+        grumpyBtn.classList.add("okay-status");
+      }
+    }
+    else {
+      grumpyBtn.textContent = "Mister Grumpy idk";
+      grumpyBtn.classList.add("unknown-status");
+    }
+  }
 };
 
-let fetchMusicVideoStatus = () => {
-  fetch("http://localhost:8080/youtube/music/find", {
+let fetchSearchMusicVideo = () => {
+  console.log("Searching for " + URL);
+  fetch(SEARCH_ENDPOINT, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json",
     },
-    body: JSON.stringify({ "url": "foo" })
+    body: JSON.stringify({
+      "url": URL
+    })
   })
     .then(resp => {
       return resp.json();
     })
     .then(result => {
-      console.log(result);
+      searchStatus = result;
+      console.log("searchStatus=");
+      console.log(searchStatus);
+      updateBtnStatus();
     })
     .catch(err => {
       console.log("error");
       console.log(err);
     })
 };
+fetchSearchMusicVideo();
+
+var downloadStatus = null;
+let fetchDownloadMusicVideo = () => {
+  console.log("Downloading for " + URL);
+  fetch(DOWNLOAD_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      "url": URL
+    })
+  })
+    .then(resp => {
+      return resp.json();
+    })
+    .then(result => {
+      downloadStatus = result;
+      fetchSearchMusicVideo(); // Another to confirm the file exists
+    })
+    .catch(err => {
+      console.log("error");
+      console.log(err);
+    })
+}
+
+let injectMisterGrumpy = () => {
+  grumpyBtn = document.createElement("button");
+  grumpyBtn.id = "mister-grumpy-button"
+  grumpyBtn.textContent = "Mister Grumpy's button"
+  grumpyBtn.onclick = fetchDownloadMusicVideo;
+
+  let infoTextNode = document.querySelector("#info-text");
+  infoTextNode.parentNode.insertBefore(grumpyBtn, infoTextNode.nextSibling);
+  
+  updateBtnStatus();
+};
 
 let id = setInterval(() => {
   let infoTextNode = document.querySelector("#info-text");
   if (infoTextNode) {
+    console.log("Inserting Mister Grumpy's needle...");
     injectMisterGrumpy();
     console.log("Removed Mr. Grumpy's needle!");
     clearInterval(id);

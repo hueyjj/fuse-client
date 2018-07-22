7f4205d291384d51bc0859a4d8352b3bbfeb67d3
hueyjj <jasperjeng@gmail.com>
2018-07-22 16:15:59 -0700

    Added replay functionality Added replay button When video changes it play button to a replay button, the replay functionality will fire and restart the video

--- chrome-ext/youtube-dl-ext/content-script.js
+++ chrome-ext/youtube-dl-ext/content-script.js
@@ -31,6 +31,9 @@ const MUSIC_EXIST = "MUSIC_EXIST";
 const DOWNLOAD_FAIL = "DOWNLOAD_FAIL";
 const DOWNLOAD_SUCCESS = "DOWNLOAD_SUCCESS";
 let URL = null;
+let shouldReplay = false;
+
+let replayBtn = null;
 
 var grumpyBtn = null;
 var searchStatus = null; 
@@ -132,15 +135,43 @@ let fetchDownloadMusicVideo = () => {
 }
 
 let injectMisterGrumpy = () => {
-  grumpyBtn = document.createElement("button");
-  grumpyBtn.id = "mister-grumpy-button"
-  grumpyBtn.classList.add("grumpy-button");
-  grumpyBtn.textContent = "Mister Grumpy's button"
-  grumpyBtn.onclick = fetchDownloadMusicVideo;
+  replayBtn = document.createElement("button");
+  replayBtn.id = "23958h2-replay-button";
+  replayBtn.classList.add("hueyjj-replay-button");
+  replayBtn.onclick = () => {
+    if (shouldReplay) {
+      replayBtn.classList.remove("replay-enable");
+      replayBtn.classList.add("replay-disable");
+      shouldReplay = false;
+    } else {
+      replayBtn.classList.remove("replay-disable");
+      replayBtn.classList.add("replay-enable");
+      shouldReplay = true;
+    }
+  }
+
+  let replaySvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  replaySvg.width = "100%";
+  replaySvg.height = "100%";
+  replaySvg.setAttribute("viewBox", "0 0 36 36");
+  let replayPathSvg = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  replayPathSvg.classList.add("replay-path-svg");
+  replayPathSvg.setAttribute("d", "M 18,11 V 7 l -5,5 5,5 v -4 c 3.3,0 6,2.7 6,6 0,3.3 -2.7,6 -6,6 -3.3,0 -6,-2.7 -6,-6 h -2 c 0,4.4 3.6,8 8,8 4.4,0 8,-3.6 8,-8 0,-4.4 -3.6,-8 -8,-8 z");
+
+  replaySvg.appendChild(replayPathSvg);
+  replayBtn.appendChild(replaySvg);
 
   let infoTextNode = document.querySelector("#info-text");
+  infoTextNode.parentNode.insertBefore(replayBtn, infoTextNode.nextSibling);
+
+  grumpyBtn = document.createElement("button");
+  grumpyBtn.id = "mister-grumpy-button";
+  grumpyBtn.classList.add("grumpy-button");
+  grumpyBtn.textContent = "Mister Grumpy's button";
+  grumpyBtn.onclick = fetchDownloadMusicVideo;
+
   infoTextNode.parentNode.insertBefore(grumpyBtn, infoTextNode.nextSibling);
-  
+
   updateBtnStatus();
 };
 
@@ -150,6 +181,25 @@ let id = setInterval(() => {
     console.log("Inserting Mister Grumpy's needle...");
     injectMisterGrumpy();
     console.log("Removed Mr. Grumpy's needle!");
+  }
+
+  let playButton = document.querySelector(".ytp-play-button");
+  if (playButton) {
+    let config = { attributes: true };
+    let callback = (mutationsList) => {
+      for (let mutation of mutationsList) {
+        if (mutation.type == "attributes"
+          && mutation.target.title.toLowerCase() == "replay"
+          && shouldReplay) {
+            mutation.target.click();
+        }
+      }
+    }
+    let observer = new MutationObserver(callback);
+    observer.observe(playButton, config);
+  }
+
+  if (infoTextNode && playButton) {
     clearInterval(id);
   }
 }, 100);
